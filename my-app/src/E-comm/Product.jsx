d36b6a3e7582a3e55f44682bd75cb8edf64f5a77
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2024-01-10 00:11:54 +0530

    ..

--- my-app/src/E-comm/Product.jsx
+++ my-app/src/E-comm/Product.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+
+import Swal from "sweetalert2";
 import "./product.css";
 
 function Product({ product, id }) {
   const [ITEm, setITEm] = useState(false);
-  // const [quantity, setquantity] = useState(1);
+  const [quantity, setquantity] = useState(1);
 
   const addcart = (item) => {
-    let localData = { ...item, quantity: 1 };
+    let localData = { ...item, quantity:1 };
     let getData = JSON.parse(localStorage.getItem("cartData")) || [];
     if (!getData.some((item) => item.id === id)) {
       let localArr = getData.concat(localData);
@@ -19,13 +21,29 @@ function Product({ product, id }) {
     }
   };
 
-  const Details = (item) => {
-    localStorage.setItem("More-Detail", JSON.stringify(item));
+  // const Details = (item) => {
+  //   localStorage.setItem("More-Detail", JSON.stringify(item));
+  // };
+
+  const showDetailsAlert = (item) => {
+    Swal.fire({
+      title: item.name,
+      html: `
+      <img src=${item.thumbnail} alt="..." className="Sweet-Alert-Img"/>
+        <p>Description: ${item.description}</p>
+        <p>Price: $${item.price}</p>
+        
+      `,
+      icon: "image",
+    });
   };
 
   return (
     <div className="text-center">
-      <div className="product-profile" onClick={() => Details(product)}>
+      <div
+        className="product-profile"
+        onClick={() => showDetailsAlert(product)}
+      >
         <img src={product.images[0]} alt={product.title} />
         <h2>{product.title}</h2>
         <p>{product.description}</p>
